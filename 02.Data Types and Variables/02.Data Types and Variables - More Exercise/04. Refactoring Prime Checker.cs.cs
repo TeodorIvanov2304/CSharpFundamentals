3d5a99ea1321ea2bdd7297b71fa5dece4edfd378
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-27 12:49:23 +0300

    Update 04. Refactoring Prime Checker.cs.cs

--- 02.Data Types and Variables/02.Data Types and Variables - More Exercise/04. Refactoring Prime Checker.cs.cs
+++ 02.Data Types and Variables/02.Data Types and Variables - More Exercise/04. Refactoring Prime Checker.cs.cs
@@ -1,31 +1,28 @@
 ﻿using System;
-namespace _03._Floating_Equality
+using System.Reflection;
+
+namespace _04._Refactoring_Prime_Checker
 {
-    internal class FloatingEquality
+    internal class RefactoringPrimeChecker
     {
         static void Main(string[] args)
         {
-            // Write a program that safely compares floating - point numbers(double) with precision eps = 0.000001.Note that we cannot directly compare two floating - point numbers a and b by a == b, because of the nature of the floating - point arithmetic.Therefore, we assume two numbers are equal if they are more close to each other than some fixed constant eps.
-            //You will receive two lines, each containing a floating-point number.Your task is to compare the values of the two numbers.
+            //You are given a program that checks if numbers in a given range[2...N] are prime. For each number is printed "{number} -> {true or false}".The code, however, is not very well written.Your job is to modify it in a way that is easy to read and understand.
 
-
-            double numberA = double.Parse(Console.ReadLine());
-            double numberB = double.Parse(Console.ReadLine());
-            double eps = 0.000001;
-
-            //Тук се използва Math.Abs за да се изчисли абсолютната стойност на разликата между двата double числа, и след това се сравнява тази разлика с предварително зададената точност epsilon. Ако абсолютната разлика е по-малка от epsilon, тогава числата се считат за равни с точност до 0.000001.
-
-            if (Math.Abs(numberA - numberB)<eps) 
+            int upperBound = int.Parse(Console.ReadLine());
+            for (int startingNumber = 2; startingNumber <= upperBound; startingNumber++)
             {
-                Console.WriteLine("True");
-                //Console.WriteLine($"{Math.Abs(numberA - numberB):F8}");
+                bool isPrime = true;
+                for (int divisor = 2; divisor < startingNumber; divisor++)
+                {
+                    if (startingNumber % divisor == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{startingNumber} -> {isPrime.ToString().ToLower()}");
             }
-            else
-            {
-                Console.WriteLine("False");
-                //Console.WriteLine($"{Math.Abs(numberA - numberB):F8}");
-            }
-
         }
     }
 }
