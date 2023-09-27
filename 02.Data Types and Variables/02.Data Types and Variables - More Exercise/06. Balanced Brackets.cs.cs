1570b61e97970fb780649b2aac4065ea19131306
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-27 16:18:49 +0300

    Create 06. Balanced Brackets.cs.cs

---
+++ 02.Data Types and Variables/02.Data Types and Variables - More Exercise/06. Balanced Brackets.cs.cs
@@ -0,0 +1,37 @@
+﻿namespace _06._Balanced_Brackets
+{
+    internal class BalancedBrackets
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int openingB = 0;
+            int closingB = 0;
+            string lastString = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string nextString = Console.ReadLine();
+                lastString = nextString;
+                if (nextString == "(")
+                {
+                    openingB++;
+                    lastString = nextString;
+                }
+                else if (nextString == ")")
+                {
+                    closingB++;
+                    lastString = nextString;
+                }
+            }
+            // Ако string-ът завършва с отворена скоба "(", има възможност скобите да са еднакъв брой но пак да даде грешка, защото скобите ще са обратни 
+            if (openingB == closingB&&lastString!="(")
+            {
+                Console.WriteLine("BALANCED");
+            }
+            else
+            {
+                Console.WriteLine("UNBALANCED");
+            }
+        }
+    }
+}
