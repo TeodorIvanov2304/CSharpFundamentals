317ee444c4d494da5860f821d34c427d89dbb3cd
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-28 19:43:55 +0300

    Created 04.Array folder

---
+++ 04.Arrays/04.Arrays - Lab/01. Day of Week.cs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { 
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+            };
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number) 
+            {
+                case 1: Console.WriteLine(days[0]); break;
+                case 2: Console.WriteLine(days[1]); break;
+                case 3: Console.WriteLine(days[2]);break;
+                case 4: Console.WriteLine(days[3]); break;
+                case 5: Console.WriteLine(days[4]); break;
+                case 6: Console.WriteLine(days[5]); break;
+                case 7: Console.WriteLine(days[6]); break;
+                default: Console.WriteLine("Invalid day!");
+                    break;
+            }
+        }
+    }
+}
