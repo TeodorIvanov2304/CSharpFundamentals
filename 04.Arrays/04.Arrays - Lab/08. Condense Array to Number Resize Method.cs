3d93b802c69381a12c1d710517b959f2dda7090a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-30 20:57:16 +0300

    Create 08. Condense Array to Number Resize Method.cs

---
+++ 04.Arrays/04.Arrays - Lab/08. Condense Array to Number Resize Method.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._Condense_Array_to_Number_Resize_Method
+{
+    internal class CondenseArrayТoNumberResizeMethod
+    {
+        static void Main(string[] args)
+        {   
+            //Създаваме масив, който четем от конзолата
+            int[] array = Console.ReadLine()
+                          .Split()
+                          .Select(int.Parse)
+                          .ToArray();
+
+            //Създаваме нов масив, който да пази събраните стойности
+            int[] condensedArray = new int[array.Length - 1];
+
+            //Създавме while цикъл, който върти до array.Length > 1
+            while (array.Length > 1)
+            {   
+                //Създаваме for цикъл който върти до дължината - 1
+                for (int i = 0; i < array.Length - 1; i++)
+                {   
+                    //Събираме кондензирания масив, с итерациите от масива array[i] + array[i+1]
+                    condensedArray[i] = array[i] + array[i + 1];
+                    array[i] = condensedArray[i];
+                }
+                //Използваме класът System.Array и функцията му Resize(реферираме масива, дължината на масива - 1)
+                Array.Resize(ref condensedArray, condensedArray.Length - 1);
+                Array.Resize(ref array, array.Length - 1);
+            }
+
+            //Създаваме foreach цикъл за да изпирнтим резултата
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
