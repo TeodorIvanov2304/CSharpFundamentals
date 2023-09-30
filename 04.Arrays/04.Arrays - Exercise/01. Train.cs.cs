6be9ec8c0120d49ddba6639781b04fe5e08b191e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-30 13:47:45 +0300

    Create 01. Train.cs.cs

---
+++ 04.Arrays/04.Arrays - Exercise/01. Train.cs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._Day_of_Week_IF_ELSE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+            };
+            int number = int.Parse(Console.ReadLine());
+
+            if(number<1||number>7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(days[number-1]);
+            }
+        }
+    }
+}
