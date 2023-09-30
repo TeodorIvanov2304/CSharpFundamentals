a1daf04aa1f4320907f36014699d473642bc2e48
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-30 13:56:49 +0300

    Create 06. Even and Odd Subtraction.cs.cs

---
+++ 04.Arrays/04.Arrays - Lab/06. Even and Odd Subtraction.cs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Sum_Even_Numbers
+{
+    internal class SumEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            //Read an array from the console and sum only its even values. 
+
+            // Четем числата от конзолата и ги запазваме в масив int
+            int[] numbers = Console.ReadLine()
+                            .Split()
+                            .Select(int.Parse)
+                            .ToArray();
+
+            // Създаваме променлива, която пази сумата на четните числа
+            int sum = 0;
+
+            //Създаване цикъл, който върти от 0 до края на масива(numbers.Length)
+            for (int i = 0; i < numbers.Length; i++)
+            {   
+                //Създаваме променлива, която пази текущото число
+                int currentNumber = numbers[i];
+
+                // Проверяваме дали текущото число е четно
+                if(currentNumber%2==0)
+                {   
+                    // Ако да, добавяме го към сумата
+                    sum += currentNumber;
+                }
+            }
+
+            //Изпечатваме сумата
+            Console.WriteLine(sum);
+        }
+    }
+}
