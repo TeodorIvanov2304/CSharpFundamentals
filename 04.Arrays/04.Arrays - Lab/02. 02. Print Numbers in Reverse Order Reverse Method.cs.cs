29889f2b8c641f3d8b024d2c04f13250ec8bbf99
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-30 11:19:14 +0300

    Create 02. 02. Print Numbers in Reverse Order Reverse Method.cs.cs

---
+++ 04.Arrays/04.Arrays - Lab/02. 02. Print Numbers in Reverse Order Reverse Method.cs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order_Reverse_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Четем n числа от конзолата
+            int n = int.Parse(Console.ReadLine());
+
+            // Създаваме масив от стринг с дължината на n
+            int[] integerArray = new int[n];
+
+            // Създаваме цикъл, който да върти от 0 до n
+            for (int i = 0; i < n; i++)
+            {
+                // Попълваме масива с числа от конзолата
+                integerArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Изпечатваме на конзолата използвайки String.Join където в скобите слагаме " " и делегат,
+            // в случая метода Reverse() от integerArray
+            Console.WriteLine(String.Join(" ",integerArray.Reverse()));
+        }
+    }
+}
