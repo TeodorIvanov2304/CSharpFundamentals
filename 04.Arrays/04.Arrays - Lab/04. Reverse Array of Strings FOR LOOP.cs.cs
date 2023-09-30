f56d2bbc153b1867549f318561ad8a3d18712740
TeodorIvanov <131561902+TeodorIvanov2304@users.noreply.github.com>
2023-09-30 13:44:27 +0300

    Update 04. Reverse Array of Strings FOR LOOP.cs.cs

--- 04.Arrays/04.Arrays - Lab/04. Reverse Array of Strings FOR LOOP.cs.cs
+++ 04.Arrays/04.Arrays - Lab/04. Reverse Array of Strings FOR LOOP.cs.cs
@@ -1,14 +1,20 @@
-﻿using System;
-
-namespace _04._Reverse_Array_of_Strings
+using System;
+using System.Linq;
+namespace _04._Reverse_Array_of_Strings_FOR_LOOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(String.Join(" ",Console.ReadLine()
-                                                .Split()
-                                                .Reverse()));
+            string[] items = Console.ReadLine().Split(" ").ToArray();
+
+            for (int i = 0; i < items.Length/2; i++)
+            {
+                string odlElements = items[i];
+                items[i] = items[items.Length - 1 - i];
+                items[items.Length - 1 - i] = odlElements;
+            }
+            Console.WriteLine(string.Join(" ", items));
         }
     }
 }
