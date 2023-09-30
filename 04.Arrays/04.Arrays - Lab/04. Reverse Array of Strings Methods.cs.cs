19c2d6b2e7ce80e94d818112d65521ec12d22455
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-09-30 13:43:14 +0300

    Adding new problem solutions

---
+++ 04.Arrays/04.Arrays - Lab/04. Reverse Array of Strings Methods.cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(" ",Console.ReadLine()
+                                                .Split()
+                                                .Reverse()));
+        }
+    }
+}
