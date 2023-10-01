8000afc7d0a00fad714576514c67949ce085babc
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-01 12:28:54 +0300

    Created 02. Common Elements Foreach and Intersect

---
+++ 04.Arrays/04.Arrays - Exercise/02. Common Elements Foreach.cs.cs
@@ -0,0 +1,25 @@
+﻿namespace _02._Common_Elements_Foreach
+{
+    internal class CommonElementsForeach
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that prints out all common elements in two arrays. You have to compare the elements of the second array to the elements of the first.
+
+            string[] arrayOne = Console.ReadLine().Split();
+            string[] arrayTwo = Console.ReadLine().Split();
+
+            foreach (string match in arrayTwo) 
+            {
+                for (int i = 0; i < arrayOne.Length; i++)
+                {
+                    if(match == arrayOne[i])
+                    {
+                        Console.Write($"{match} ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04.Arrays/04.Arrays - Exercise/02. Common Elements Intersect.cs.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._Common_Elements
+{
+    internal class CommonElementsIntersect
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that prints out all common elements in two arrays. You have to compare the elements of the second array to the elements of the first.
+
+            //Четене на string масив от конзолата
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            //Сравняване на двата масива с метода Intersect
+            string[] matchingWords = firstArray.Intersect(secondArray).ToArray();
+
+            //Изпечатване с foreach
+            foreach (string word in matchingWords) 
+            {
+                Console.Write($"{word} ");
+            }
+                                  
+        }
+    }
+}
