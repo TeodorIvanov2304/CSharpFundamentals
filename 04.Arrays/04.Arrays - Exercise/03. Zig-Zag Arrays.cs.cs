565224147cefde91a98b9838222026889b87104f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-01 18:33:19 +0300

    Created 03. Zig-Zag Array and 04. Array Rotations

---
+++ 04.Arrays/04.Arrays - Exercise/03. Zig-Zag Arrays.cs.cs
@@ -0,0 +1,52 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Metadata;
+using System;
+
+namespace _03._Zig_Zag_Arrays
+{
+    internal class ZigZagArrays
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that creates 2 arrays.You will be given an integer n.On the next n lines, you will get 2 integers.Form 2 new arrays in a zig-zag pattern as shown below.
+            //Examples
+            //Input   Output
+            //4       1 10 31 20
+            //1 5     5 9 81 41
+            //9 10
+            //31 81
+            //41 20
+
+            //Четем n редове от конзолата
+            int n = int.Parse(Console.ReadLine());
+
+            //Създаване два масива с дължина n
+            int[] zigZagOne = new int[n];
+            int[] zigZagTwo = new int[n];
+
+            //Създаваме цикъл, който върти n пъти
+            for (int i = 0; i < n; i++)
+            {   
+                //Създаваме string масив , който четем от конзолата и сплитваме по " ".
+                string[] input = Console.ReadLine().Split();
+
+                //Правим проверка дали реда на четене от конзолата е четен или не
+                if(i%2==0)
+                {   
+                    //Ако да, прибавяме първият елемент [0] към втория масив. Ако не, прибавяме първият елемент [0] към първият масив и обратно
+                    zigZagOne[i] += int.Parse(input[0]);
+                    zigZagTwo[i] += int.Parse(input[1]);
+                }
+                else
+                {
+                    zigZagOne[i] += int.Parse(input[1]);
+                    zigZagTwo[i] += int.Parse(input[0]);
+                }
+            }
+            //Изпечатваме на конзолата
+            Console.WriteLine(string.Join(" ", zigZagOne));
+            Console.WriteLine(string.Join(" ", zigZagTwo));
+
+        }
+    }
+}
---
+++ 04.Arrays/04.Arrays - Exercise/04. Array Rotation.cs.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Linq;
+
+namespace _04._Array_Rotation
+{
+    internal class ArrayRotation
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that receives an array and several rotations that you have to perform.The rotations are done by moving the first element of the array from the front to the back. Print the resulting array.
+
+            //Четем string масив от конзолата
+            string[] array = Console.ReadLine()
+                          .Split();
+            //Четем броят ротации
+            int rotations = int.Parse(Console.ReadLine());
+            //Създаваме цикъл, който ще върти ротациите
+            for (int i = 0; i < rotations; i++)
+            {
+                //Създаваме променлива, която ще пази 0-евият елемент, който ще се върти
+                string elementToRotate = array[0];
+
+                //Започваме брояча от 1, защото вече сме взели първият елемент в отделна променлива
+                for (int j = 1; j < array.Length; j++)
+                {   
+                    //Взимаме сегашният елемент и го записваме в променлива
+                    string currentElement = array[j];
+
+                    //Връщаме местата на елементите с едно назад
+                    array[j-1] = currentElement;
+                }
+                //Слагаме 0-евият елемент на последно място
+                array[array.Length-1] = elementToRotate;
+            }
+
+            Console.WriteLine(string.Join(" ", array));
+
+        }
+    }
+}
