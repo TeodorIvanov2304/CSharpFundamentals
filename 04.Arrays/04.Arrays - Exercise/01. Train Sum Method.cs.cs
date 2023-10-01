8673d5af1555fa2bd3a0d21379c2b746be1eefec
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-01 11:23:16 +0300

    Created 01. Train and Train Sum Method

--- 04.Arrays/04.Arrays - Exercise/01. Train Sum Method.cs.cs
+++ 04.Arrays/04.Arrays - Exercise/01. Train Sum Method.cs.cs
@@ -1,4 +1,4 @@
-﻿namespace _01._Train
+﻿namespace _01._Train_Sum_Method
 {
     internal class Program
     {
@@ -7,14 +7,13 @@
             //A train has n number of wagons(integer, received as input).On the next n lines, you will receive the number of people that are going to get on each wagon. Print out the number of passengers in each wagon followed by the total number of passengers on the train.
 
             int wagons = int.Parse(Console.ReadLine());
-            int sum = 0;
             int[] peopleInWagon = new int[wagons];
             for (int i = 0; i < wagons; i++)
             {
                 peopleInWagon[i] = int.Parse(Console.ReadLine());
-                sum += peopleInWagon[i];
             }
             Console.WriteLine(String.Join(" ", peopleInWagon));
+            int sum = peopleInWagon.Sum();
             Console.WriteLine(sum);
         }
     }
