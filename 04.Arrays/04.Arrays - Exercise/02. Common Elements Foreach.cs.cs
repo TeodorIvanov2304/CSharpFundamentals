fb8a3d6df58c0914d131d2bfc36d390a250834e1
TeodorIvanov <131561902+TeodorIvanov2304@users.noreply.github.com>
2023-10-01 12:32:06 +0300

    Update 02. Common Elements Foreach.cs.cs

--- 04.Arrays/04.Arrays - Exercise/02. Common Elements Foreach.cs.cs
+++ 04.Arrays/04.Arrays - Exercise/02. Common Elements Foreach.cs.cs
@@ -6,15 +6,21 @@
         {
             //Create a program that prints out all common elements in two arrays. You have to compare the elements of the second array to the elements of the first.
 
+            //Четем двата string масива от конзолата
             string[] arrayOne = Console.ReadLine().Split();
             string[] arrayTwo = Console.ReadLine().Split();
 
+            //Създаваме цикъл foreach, който върти по втория масив
             foreach (string match in arrayTwo) 
-            {
+            {   
+
+                //Създаваме втори for цикъл, който върти по дължината на първия масив
                 for (int i = 0; i < arrayOne.Length; i++)
-                {
+                {    
+                    //Правим проверка, за да видим дали има съвпдащи елементи
                     if(match == arrayOne[i])
-                    {
+                    {   
+                        //Ако има, ги изпечатваме на конзолата и brake-ваме итерацията
                         Console.Write($"{match} ");
                         break;
                     }
