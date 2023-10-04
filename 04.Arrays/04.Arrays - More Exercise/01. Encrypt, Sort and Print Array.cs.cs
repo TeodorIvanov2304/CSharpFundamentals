b0b2e9e35d64614b3dd8ae0ab1b28dfb4a9d6962
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-04 13:42:51 +0300

    Created More Exercise Folder and EncryptSortPrintArray

---
+++ 04.Arrays/04.Arrays - More Exercise/01. Encrypt, Sort and Print Array.cs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    internal class EncryptSortPrintArray
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStrings = int.Parse(Console.ReadLine());
+
+            string[] arr = new string[numberOfStrings];
+
+            int[] currentArray = new int[numberOfStrings];
+
+            int sum = 0;
+
+
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+
+                arr[i] = Console.ReadLine();
+
+                string currentName = arr[i];
+
+                sum = 0;
+
+                for (int j = 0; j < currentName.Length; j++)
+                {
+
+                    char currentLetter = currentName[j];
+                    int value = (int)currentLetter;
+                    bool isVowel = "aeiouAEIOU".Contains(currentLetter);
+
+                    if (isVowel)
+                    
+                    {
+                        sum += value * currentName.Length;
+
+                    }
+                    else
+                    {
+                        sum += value / currentName.Length;
+                    }
+
+
+                }
+                currentArray[i] = sum;
+
+
+
+            }
+            Array.Sort(currentArray);
+            foreach (int item in currentArray)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
