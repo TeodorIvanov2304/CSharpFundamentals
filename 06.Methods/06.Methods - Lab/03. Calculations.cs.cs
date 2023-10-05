798eef78a0d0036432c5b2d8564d95973b1ff9c4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-05 12:57:11 +0300

    Create 03. Calculations.cs.cs

---
+++ 06.Methods/06.Methods - Lab/03. Calculations.cs.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace _02._Grades
+{
+    internal class Grades
+    {
+        static void Main(string[] args)
+        {
+            //Create a method that receives a grade between 2.00 and 6.00 and prints the corresponding grade definition:
+            //• 2.00 – 2.99 - "Fail"
+            //• 3.00 – 3.49 - "Poor"
+            //• 3.50 – 4.49 - "Good"
+            //• 4.50 – 5.49 - "Very good"
+            //• 5.50 – 6.00 - "Excellent"
+
+            double grade = double.Parse(Console.ReadLine());
+            PrintGrade(grade);
+
+        }
+
+        static void PrintGrade(double grade)
+        {
+            //Logic for calculating result
+
+            switch(grade) // Using switch-case for determining the grade 
+            {
+                case >= 2 and <= 2.99: // Използваме ключовата дума AND и така се използва bool в switch
+                    Console.WriteLine("Fail");
+                    break;
+                case >= 3 and <= 3.49: // На края на реда се слагат 2-те точки
+                    Console.WriteLine("Poor");
+                    break;
+                case >= 3.50 and <= 4.49:
+                    Console.WriteLine("Good");
+                    break;
+                case >= 4.50 and <= 5.49:
+                    Console.WriteLine("Very good");
+                    break;
+                case >= 5.50 and <= 6.00:
+                    Console.WriteLine("Excellent");
+                    break;
+            }
+        }
+    }
+}
