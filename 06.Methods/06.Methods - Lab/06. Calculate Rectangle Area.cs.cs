62541f9c39ab10947e24d280004396711952e11e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-05 17:29:35 +0300

    Create 06. Calculate Rectangle Area.cs.cs

---
+++ 06.Methods/06.Methods - Lab/06. Calculate Rectangle Area.cs.cs
@@ -0,0 +1,27 @@
+﻿namespace _06._Calculate_Rectangle_Area
+{
+    internal class CalculateRectangleArea
+    {
+        static void Main(string[] args)
+        {
+            //Четем страните на четириъгълника от конзолата
+            double width = int.Parse(Console.ReadLine()) ;
+            double length = int.Parse(Console.ReadLine());
+
+            //Запазваме резултата от метода в променлива, area. 
+            double area = CalculateRectangleAreaMethod(width,length); 
+
+            //Изпечатваме резултата (return) на конзолата
+            Console.WriteLine(area);
+
+        }
+
+        //Метод за изчисляване на лицето на четириъгълник area = a * b
+        static double CalculateRectangleAreaMethod(double width, double length)
+        {
+            //Връщаме резултата от умножението
+            return width * length;
+        }
+
+    }
+}
