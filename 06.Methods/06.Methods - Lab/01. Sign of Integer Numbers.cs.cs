d4d008c7579a55cbee537187aeaaa799c2761c8d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-05 12:16:17 +0300

    Creating Folder for Methods and 01. Sign of integer numbers

---
+++ 06.Methods/06.Methods - Lab/01. Sign of Integer Numbers.cs.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A single integer is given, create a method that checks if the given number is positive, negative, or zero. As a result print:
+            //• For positive number: "The number {number} is positive. "
+            //• For negative number: "The number {number} is negative. "
+            //• For zero number: "The number {number} is zero. "
+
+            int numberForChecking = int.Parse(Console.ReadLine());
+            PrintNumberSign(numberForChecking);
+
+        }
+
+        private static void PrintNumberSign(int numberForChecking) //Cheking number sign
+        {
+            if (numberForChecking > 0) // Positive
+            {
+                Console.WriteLine($"The number {numberForChecking} is positive. ");
+            }
+            else if (numberForChecking < 0)  // Negative
+            {
+                Console.WriteLine($"The number {numberForChecking} is negative. ");
+            }
+            else // Zero
+            {
+                Console.WriteLine($"The number {numberForChecking} is zero. ");
+            }
+        }
+
+    }
+}
