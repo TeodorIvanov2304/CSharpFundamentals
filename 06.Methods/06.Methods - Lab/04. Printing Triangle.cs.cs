63cdb8a925b74db0d54ff89cb7713bc4ac0b7de1
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-05 14:49:13 +0300

    Create 04. Printing Triangle.cs.cs

---
+++ 06.Methods/06.Methods - Lab/04. Printing Triangle.cs.cs
@@ -0,0 +1,65 @@
+﻿namespace _04._Printing_Triangle
+{
+    internal class PrintingTriangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Четем число n
+
+            PrintTriangle(n);
+
+
+            // ТОВА Е ЦЯЛАТА ПРОГРАМА САМО В MAIN МЕТОДА
+            //*****************************************************
+            //for (int row = 1; row <= n; row++)
+            //{
+            //    for (int col = 1; col <= row; col++)   С CTRL + . разделяме на методи двата вложени цикъла
+            //    {
+            //        Console.Write(col);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int row = n - 1; row >= 1; row--)
+            //{
+            //    for (int col = 1; col <= row; col++)
+            //    {
+            //        Console.Write(col);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //*****************************************************
+        }
+
+        static void PrintTriangle(int n) // Печата целия триъгълник
+        {
+            PrintUpperPartOfTriangle(n);
+            PrintBottomPart(n);
+        }
+
+        static void PrintBottomPart(int n) // Печата долната част на триъгълника
+        {
+            for (int row = n - 1; row >= 1; row--)
+            {
+                PrintLineAndSpace(row);
+            }
+        }
+
+        static void PrintUpperPartOfTriangle(int n) // Печата горната част на триъгълника
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                PrintLineAndSpace(row);
+            }
+        }
+
+        static void PrintLineAndSpace(int row) // Печата нов ред и space
+        {
+            for (int col = 1; col <= row; col++) //С CTRL + .разделяме на методи двата вложени цикъла
+            {
+                Console.Write(col + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
