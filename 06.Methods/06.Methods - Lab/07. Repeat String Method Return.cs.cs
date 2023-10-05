1e8f3d78c3057ee2c035568f680f63ddc386987a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-05 17:49:40 +0300

    Create 07. Repeat String Method Return.cs.cs

---
+++ 06.Methods/06.Methods - Lab/07. Repeat String Method Return.cs.cs
@@ -0,0 +1,24 @@
+﻿namespace _07._Repeat_String_Method_Return
+{
+    internal class RepeatStringMethodReturn
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(text, n);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatString(string text, int repeats)
+        {
+            string newText = "";
+            for (int i = 0; i < repeats; i++)
+            {
+                newText += text;
+            }
+            return newText;
+        }
+    }
+}
