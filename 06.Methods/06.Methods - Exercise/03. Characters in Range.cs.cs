71279e5e441790ae265da6346ac4f41e5e15eb59
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-06 23:15:23 +0300

    Create 03. Characters in Range.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/03. Characters in Range.cs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    internal class CharsInRange
+    {
+        static void Main(string[] args)
+        {
+            // Create a method that receives two characters and prints all the characters between them according to ASCII(on a single line).
+            //NOTE: If the second letter's ASCII value is less than that of the first one, then the two initial letters should be swapped.
+
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            char swap = ' ';
+
+            //Swap chars if needed
+            if (secondSymbol < firstSymbol)
+            {
+                swap = firstSymbol;
+                firstSymbol = secondSymbol;
+                secondSymbol = swap;
+            }
+            //Call the method
+            IterrateSymbols(firstSymbol, secondSymbol);
+        }
+
+        //Print all chars between two symbols
+        public static void IterrateSymbols(char firstSymbol, char secondSymbol)
+        {
+            for (char i = (char)(firstSymbol + 1); i < secondSymbol; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
