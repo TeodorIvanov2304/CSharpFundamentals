6ec4af7f48424a1a0e7f9ac4b8689636df60ffd0
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-06 11:28:12 +0300

    Update 09. Greater of Two Values.cs.cs

--- 06.Methods/06.Methods - Lab/09. Greater of Two Values.cs.cs
+++ 06.Methods/06.Methods - Lab/09. Greater of Two Values.cs.cs
@@ -1,28 +1,56 @@
-﻿namespace _08._Math_Power
+﻿namespace _09._Greater_of_Two_Values
 {
-    internal class MathPowerMethod
+    internal class Program
     {
         static void Main(string[] args)
         {
-            // Create a method, which receives two numbers as parameters:
-            //• The first number – the base
-            //• The second number – the power
-            //The method should return the base raised to the given power.
+            //You are given an input of two values of the same type. The values can be of type int, char or string.Create methods called GetMax(), which can compare int, char or string and returns the biggest of the two values.
 
+            string type = Console.ReadLine();
 
-            double number = double.Parse(Console.ReadLine());
-            double power = double.Parse(Console.ReadLine());
+            // Правим проверка за string type
+            if(type == "int")
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                int result = GetMax(a, b); // Method calling
+                Console.WriteLine(result);
 
-            double result = RaiseToPower(number, power);
-            Console.WriteLine(result);
+            }
+            else if(type == "char")
+            {
+                char a = char.Parse(Console.ReadLine());
+                char b = char.Parse(Console.ReadLine());
+                char result = GetMax(a, b); // Method calling
+                Console.WriteLine(result);
 
+            }
+            else if(type == "string")
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                string result = GetMax(a, b); // Method calling
+                Console.WriteLine(result);
+
+            }
         }
 
-        static double RaiseToPower(double number, double power)
+        
+        // ================================ METHOD OVERLOADS ===============================================
+        static int GetMax(int a, int b) // Проверява дали числото а > b int
         {
-            double result = 0d;
-            result = Math.Pow(number, power);
-            return result;
+            return a > b ? a : b;
         }
+
+        static char GetMax(char a, char b) //Проверява дали символът е > от другия символ по ASCII char
+        {
+            return a > b ? a : b;
+        }
+
+        static string GetMax(string a, string b) // Проверява дали string а > b string
+        {
+            return a.Equals(b) ? a : b;
+        }
+        //==================================================================================================
     }
 }
