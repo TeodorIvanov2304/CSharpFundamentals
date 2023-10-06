92c3fd3672b2f152cf4d044f20ef58b403f199fe
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-07 02:08:50 +0300

    Create 05. Add and Subtract.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/05. Add and Subtract.cs.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Add_and_Subtract
+{
+    internal class AddAndSubtract
+    {
+        static void Main(string[] args)
+        {
+            //You will receive 3 integers.Create a method that returns
+            //the sum of the first two integers and another method that subtracts
+            //the third integer from the result of the sum method.
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int addedNumbers = AddNumbers(firstNumber, secondNumber);
+
+            Console.WriteLine(SubstractNumbers(thirdNumber, addedNumbers));
+        }
+
+        public static int SubstractNumbers(int thirdNumber, int afterAdd)
+        {
+            int finalSum = afterAdd - thirdNumber;
+            return finalSum;
+        }
+
+        public static int AddNumbers(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+    }
+}
