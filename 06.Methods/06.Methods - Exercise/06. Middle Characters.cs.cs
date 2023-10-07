a2fdaeb24756f64ab966779a1cd07aa59731b11a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-07 10:22:45 +0300

    Create 06. Middle Characters.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/06. Middle Characters.cs.cs
@@ -0,0 +1,28 @@
+﻿namespace _06._Middle_Characters
+{
+    internal class MiddleCharacters
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintStringMiddleLetter(input);
+
+        }
+
+        public static void PrintStringMiddleLetter(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == input.Length / 2 && input.Length % 2 == 0)
+                {
+                    Console.WriteLine($"{input[i - 1]}{input[i]}");
+                }
+                else if (i == input.Length / 2 && input.Length % 2 != 0)
+                {
+                    Console.WriteLine(input[i]);
+                }
+
+            }
+        }
+    }
+}
