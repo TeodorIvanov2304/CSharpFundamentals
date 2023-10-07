225992ffe2616125fec3624cd044fd1e27a64a0f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-07 11:24:12 +0300

    Create 07. NxN Matrix.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/07. NxN Matrix.cs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class NxNMatrix
+    {
+        static void Main(string[] args)
+        {
+            //Create a method that receives a single integer n and prints an NxN matrix using this number as a filler.
+
+            int MatrixBase = int.Parse(Console.ReadLine());
+            int[] matrix = new int[MatrixBase];
+
+            FillMatrixWithN(MatrixBase, matrix);
+            PrintMatrix(MatrixBase, matrix);
+
+        }
+
+        private static void PrintMatrix(int MatrixBase, int[] matrix)
+        {
+            for (int i = 0; i < MatrixBase; i++)
+            {
+                PrintMatrixRow(matrix);
+            }
+        }
+
+        private static void PrintMatrixRow(int[] matrix)
+        {
+            Console.WriteLine(string.Join(" ", matrix));
+        }
+
+        private static void FillMatrixWithN(int MatrixBase, int[] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = MatrixBase;
+            }
+        }
+    }
+}
