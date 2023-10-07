2baf217d9cf0ad65956d561c1927dedfe105789f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-07 11:53:40 +0300

    Create 08. Factorial Division.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/08. Factorial Division.cs.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace _08._Factorial_Division
+{
+    internal class FactorialDivision
+    {
+        static void Main(string[] args)
+        {
+            //Read two integers.Calculate the factorial of each number. Divide the first result by the second and print the result of the division formatted to the second decimal point.
+
+            //Creating DECIMAL variables, becasuse decimal is more accurate
+            decimal firstNumber = decimal.Parse(Console.ReadLine());
+            decimal secondNumber = decimal.Parse(Console.ReadLine());
+
+            firstNumber = FindFactorial(firstNumber);
+            secondNumber = FindFactorial(secondNumber);
+
+            decimal result = Divide(firstNumber, secondNumber);
+
+            PrintResult(result);
+        }
+        //PRINT FORMATTED RESULT
+        public static void PrintResult(decimal result)
+        {
+            Console.WriteLine($"{result:F2}");
+        }
+
+        //FIND RESULT BY DIVIDE
+        public static decimal Divide(decimal firstNumber, decimal secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+
+        //FIND FACTORIAL
+        public static decimal FindFactorial(decimal firstNumber)
+        {
+            decimal factoriel = 1;
+            while (true)
+            {
+                if (firstNumber <= 1)
+                {
+                    break;
+                }
+                factoriel = factoriel * firstNumber;
+                firstNumber--;
+            }
+
+            return factoriel;
+        }
+    }
+}
