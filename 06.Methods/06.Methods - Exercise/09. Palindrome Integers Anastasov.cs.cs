735e0f448a155bad6250cfd29cc8edb8b396fc96
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-08 18:43:23 +0300

    Create 09. Palindrome Integers Anastasov.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/09. Palindrome Integers Anastasov.cs.cs
@@ -0,0 +1,41 @@
+﻿namespace _09._Palindrome_Integers
+{
+    internal class PalindromeIntegers
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            input = CheksForPalindromes(input);
+        }
+
+        static string CheksForPalindromes(string input)
+        {
+            while (input != "END")
+            {
+                string reversed = string.Empty;
+                reversed = StringReverse(input, reversed);
+                bool result = input == reversed;
+                PrintResult(result);
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        static void PrintResult(bool result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static string StringReverse(string input, string reversed)
+        {
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                reversed += input[i];
+            }
+
+            return reversed;
+        }
+    }
+}
