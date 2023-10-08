5ebea4a90fe343abb2f6bd876eca6b464412d5ba
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-08 12:42:20 +0300

    Create 08. Factorial Division FOR-LOOP.cs.cs

---
+++ 06.Methods/06.Methods - Exercise/08. Factorial Division FOR-LOOP.cs.cs
@@ -0,0 +1,28 @@
+﻿namespace _08._Factorial_Division_FOR_LOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            //След като направим задачата, унищожаваме тези променливи
+            //double firstFactorial = FindFactorial(first);
+            //double secondFactorial = FindFactorial(second);
+
+            Console.WriteLine($"{FindFactorial(first) / FindFactorial(second):F2}");
+        }
+
+        private static double FindFactorial(int number)
+        {
+            double firstFactorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                firstFactorial *= i;
+            }
+
+            return firstFactorial;
+        }
+    }
+}
