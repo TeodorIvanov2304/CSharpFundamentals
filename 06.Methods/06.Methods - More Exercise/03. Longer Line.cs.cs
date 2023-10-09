5190b4cfb19a9b720ed158c6a303f21f885d86d0
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-09 12:28:39 +0300

    Update 03. Longer Line.cs.cs

--- 06.Methods/06.Methods - More Exercise/03. Longer Line.cs.cs
+++ 06.Methods/06.Methods - More Exercise/03. Longer Line.cs.cs
@@ -1,9 +1,15 @@
-﻿namespace _03._Longer_Line
+﻿using System.Numerics;
+using System;
+
+namespace _03._Longer_Line
 {
     internal class LongerLine
     {
         static void Main(string[] args)
         {
+            //You are given the coordinates of four points in the 2D plane.The first and the second pair of points form two different lines. Print the longer line in the format "(X1, Y1)(X2, Y2)" starting with the point that is closer to the center of the coordinate system(0, 0). (You can reuse the method that you wrote for the previous problem.) If the lines are of equal length, print only the first one.
+
+            //Read coordinates
             double x1 = double.Parse(Console.ReadLine());
             double y1 = double.Parse(Console.ReadLine());
             double x2 = double.Parse(Console.ReadLine());
@@ -13,10 +19,13 @@
             double x4 = double.Parse(Console.ReadLine());
             double y4 = double.Parse(Console.ReadLine());
 
+            //Find the longer line
             double resultA = FindTheLongerLine(x1, x2, y1, y2);
             double resultB = FindTheLongerLine(x3, x4, y3, y4);
+            //Creating variables to store the sums of coordinates
             double sumA = 0;
             double sumB = 0;
+            //Find the closest to the center coordinates
             FindClosestCoordinates(x1, y1, x2, y2, x3, y3, x4, y4, resultA, resultB, ref sumA, ref sumB);
         }
 
