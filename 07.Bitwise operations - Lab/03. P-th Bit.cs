a870a3450b928dd1564f2ea6835007cecec49d2f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-11 18:33:22 +0300

    Create 03. P-th Bit.cs

---
+++ 07.Bitwise operations - Lab/03. P-th Bit.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace _03._P_th_Bit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that prints the bit at position p of the given integer. We use the standard counting: from right to left, starting from 0.
+            int number = int.Parse(Console.ReadLine());
+            int position = int.Parse(Console.ReadLine());
+
+            int shiftedNumber = number >> position;
+            int leastSignificantBit = shiftedNumber & 1;
+            Console.WriteLine(leastSignificantBit);
+        }
+    }
+}
