ed3c7f2be5c139c9dad0a4bda26f68a210a3c785
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-11 18:20:15 +0300

    Create 02. Bit at Position 1.cs

---
+++ 07.Bitwise operations - Lab/02. Bit at Position 1.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace _02._Bit_at_Position_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that prints the bit at position 1 of the given integer. We use the standard counting: from right to left, starting from 0
+
+            int number = int.Parse(Console.ReadLine());
+            int shiftedNumber = number >> 1;
+
+            //Проверяваме какъв е последният символ, като добавяме 0001 (1).Ако е 1, връща 1.
+            //Ако е 0, връща 0
+            int leastSignificantBit = shiftedNumber & 1;
+
+            Console.WriteLine(leastSignificantBit);
+        }
+    }
+}
