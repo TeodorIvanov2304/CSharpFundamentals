3173a007f78f3b6d1adcd22d06dce62d8899cee3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-13 11:48:13 +0300

    Create 04. List of Products.cs

---
+++ 08.Lists/08.Lists - Lab/04. List of Products.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+
+namespace _04._List_of_Products
+{
+    internal class ListOfProducts
+    {
+        static void Main(string[] args)
+        {
+            //Read a number n and n lines of products.Print a numbered list of all the products ordered by name.
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+
+            for(int i = 0; i < n; i++) 
+            {
+                string product = Console.ReadLine();
+                products.Add(product);
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}"); //Правим го на i + 1 иначе излиза от листа
+            }
+            
+        }
+    }
+}
