0b2fb82703d983a4db98bfec699e8c2101199ddd
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-14 22:03:14 +0300

    Create 02. Change List Predikat.cs

---
+++ 08.Lists/08.Lists - Exercise/02. Change List Predikat.cs
@@ -0,0 +1,38 @@
+﻿namespace _02._Change_List_Predikat
+{
+    internal class ChangeListPredikat
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInt = Console.ReadLine()
+                                    .Split()
+                                    .Select(int.Parse)
+                                    .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command!="end") 
+            {
+                string[] commandArgs = command.Split();
+                string firstCommand = commandArgs[0];
+                int element = int.Parse(commandArgs[1]);
+
+                if (firstCommand=="Delete") 
+                {   
+                    //ПРЕДИКАТ! Намира всички елементи == х и ги премахва!
+                    listOfInt.RemoveAll(x => x == element);
+                }
+
+                else if(firstCommand == "Insert")
+                {
+                    int index = int.Parse(commandArgs[2]);
+                    listOfInt.Insert(index, element);
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", listOfInt));
+        }
+    }
+}
