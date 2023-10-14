97e9c9a65e0746c88ac30b12b21d4a5f47161678
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-14 20:53:21 +0300

    Create List Exercise folder and 01. Train

---
+++ 08.Lists/08.Lists - Exercise/01. Train.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input = "";
+
+            while ((input = Console.ReadLine())!="end") 
+            {
+                string[] inputArguments = input.Split();
+
+                if (inputArguments[0] == "Add")
+                {
+                    int passengers = int.Parse(inputArguments[1]);
+                    wagons.Add(passengers);
+                    //wagons.Add(int.Parse(arguments[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(inputArguments[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int currentWagon = wagons[i];
+                        bool isItFull = currentWagon + passengers <= maxCapacity;
+
+                        if (isItFull)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
