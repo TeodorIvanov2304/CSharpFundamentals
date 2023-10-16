e8c551b5fdef86c6638e0fc0a8c3ecc5de215aab
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-16 12:57:25 +0300

    Create 07. Append Arrays.cs

---
+++ 08.Lists/08.Lists - Exercise/07. Append Arrays.cs
@@ -0,0 +1,28 @@
+﻿namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                                        .Split(" ")
+                                        .ToList();
+            
+            numbers.Reverse();
+
+            List<string> result = new List<string>();
+
+            foreach (string number in numbers) 
+            {
+                string[] items = number.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+                foreach (string numbersToAdd in items)
+                {
+                    result.Add(numbersToAdd);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+
+
+        }
+    }
+}
