0cca7ef03c3f5c50efbc908988c015ccda4cd08e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-21 22:31:48 +0300

    Created Folder More Exercise and Messaging Problem

---
+++ 08.Lists/08.Lists - More Exercise/01. Messaging.cs
@@ -0,0 +1,39 @@
+﻿namespace _01._Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(int.Parse)
+                                       .ToList();
+            string text = Console.ReadLine();
+            
+            string finalText = "";
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int sum = 0;
+                string sums = numbers[i].ToString();
+                for (int j = 0; j < sums.Length; j++)
+                {
+                    int currentNumber = int.Parse(sums[j].ToString());
+                    sum += currentNumber;
+                }
+
+                int index = sum;
+
+                if(index>text.Length)
+                {
+                    index = index - text.Length;
+                }
+
+                finalText += text[index];
+                text = text.Remove(index, 1);
+                
+            }
+            Console.WriteLine(finalText);
+
+        }
+    }
+}
