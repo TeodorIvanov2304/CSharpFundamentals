0933bb481b5606a0fe5c0438b1e631a4be7dbc9a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-21 12:49:25 +0300

    Create 01. SoftUni Reception.cs

---
+++ 09.Mid Exam Preparation/01. SoftUni Reception.cs
@@ -0,0 +1,31 @@
+﻿namespace _01._SoftUni_Reception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte employee1Efficiency = byte.Parse(Console.ReadLine());
+            byte employee2Efficiency = byte.Parse(Console.ReadLine());
+            byte employee3Efficiency = byte.Parse(Console.ReadLine());
+
+            int studentsCount = int.Parse(Console.ReadLine());
+            int allEmployeesWork = employee1Efficiency + employee2Efficiency + employee3Efficiency;
+            int hoursCount = 0;
+
+            while (studentsCount > 0) 
+            {
+                hoursCount++;
+                if (hoursCount % 4 == 0 && hoursCount >0)
+                {
+                    
+                    continue;
+                }
+                
+                studentsCount -= allEmployeesWork;
+                
+            }
+
+            Console.WriteLine($"Time needed: {hoursCount}h.");
+        }
+    }
+}
