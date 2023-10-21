714f6b6d481b6f1983fef6983b55ed0f75e582ea
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-21 15:20:44 +0300

    Create 02. Shopping List.cs

---
+++ 09.Mid Exam Preparation/02. Shopping List.cs
@@ -0,0 +1,55 @@
+﻿namespace _02._Shopping_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> groceries = Console.ReadLine()
+                                            .Split("!")
+                                            .ToList();
+            string commands = "";
+
+            while ((commands = Console.ReadLine()) != "Go Shopping!")
+            {
+                string[] commandsArgs = commands.Split().ToArray();
+                string command = commandsArgs[0];
+                string item = commandsArgs[1];
+
+                switch (command)
+                {
+                    case "Urgent":
+                        if (!groceries.Contains(item))
+                        {
+                            groceries.Insert(0, item);
+                        }
+                        break;
+                    case "Unnecessary":
+                        if (groceries.Contains(item))
+                        {
+                            groceries.Remove(item);
+                        }
+                        break;
+                    case "Correct":
+                        string newItem = commandsArgs[2];
+                        if (groceries.Contains(item))
+                        {
+                            int index = groceries.IndexOf(item);
+                            groceries[index] = newItem;
+                        }
+                        break;
+                    case "Rearrange":
+                        if (groceries.Contains(item))
+                        {
+                            string itemToRearrange = item;
+                            groceries.Remove(item);
+                            groceries.Add(itemToRearrange);
+                        }
+                        break;
+
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", groceries));
+        }
+    }
+}
