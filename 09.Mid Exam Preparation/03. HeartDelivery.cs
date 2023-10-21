c11ccaf667d6323996451249708f9efcc868337c
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-21 19:06:53 +0300

    Create 03. HeartDelivery.cs

---
+++ 09.Mid Exam Preparation/03. HeartDelivery.cs
@@ -0,0 +1,54 @@
+﻿namespace HeartDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] neighborhood = Console.ReadLine().Split("@", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int lastPosition = 0;
+            int houses = 0;
+
+            while (command[0] != "Love!")
+            {
+                int length = int.Parse(command[1]);
+                lastPosition += length;
+
+                if (lastPosition < 0 || lastPosition >= neighborhood.Length)
+                {
+                    lastPosition = 0;
+                }
+
+                if (neighborhood[lastPosition] != 0)
+                {
+                    neighborhood[lastPosition] -= 2;
+
+                    if (neighborhood[lastPosition] == 0)
+                    {
+                        houses++;
+                        Console.WriteLine($"Place {lastPosition} has Valentine's day.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Place {lastPosition} already had Valentine's day.");
+                }
+
+                command = Console.ReadLine().Split().ToArray();
+            }
+
+            Console.WriteLine($"Cupid's last position was {lastPosition}.");
+
+            if (houses == neighborhood.Length)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid has failed {neighborhood.Length - houses} places.");
+            }
+        }
+    }
+}
