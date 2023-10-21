77037bd0948dbd5425b1acbb0a80fa08f9e100e2
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-21 13:21:34 +0300

    Changed Problem conditions

--- 09.Mid Exam Preparation/02. ArrayModifier.cs
+++ 09.Mid Exam Preparation/02. ArrayModifier.cs
@@ -1,6 +1,19 @@
+﻿/*
+You are given an array with integers. Write a program to modify the elements after receiving the following commands:
+•	"swap {index1} {index2}" takes two elements and swap their places.
+•	"multiply {index1} {index2}" takes element at the 1st index and multiply it with the element at 2nd index. Save the product at the 1st index.
+•	"decrease" decreases all elements in the array with 1.
+Input
+On the first input line, you will be given the initial array values separated by a single space.
+On the next lines, you will receive commands until you receive the command "end". The commands are as follows: 
+•	"swap {index1} {index2}"
+•	"multiply {index1} {index2}"
+•	"decrease"
+
+*/
 namespace _02._Array_Modifier
 {
-    internal class Program
+    internal class ArrayModifier
     {
         static void Main(string[] args)
         {
