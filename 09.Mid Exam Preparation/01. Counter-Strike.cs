447dcd068b681576b0125a5d7f5269987bb07630
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-21 14:57:13 +0300

    Create 01. Counter-Strike.cs

---
+++ 09.Mid Exam Preparation/01. Counter-Strike.cs
@@ -0,0 +1,35 @@
+﻿namespace _01._Counter_Strike
+{
+    internal class CounterStrike
+    {
+        static void Main(string[] args)
+        {
+            short energy = short.Parse(Console.ReadLine());
+            byte battlesCount = 0; 
+            string input = "";
+
+            while ((input = Console.ReadLine()) != "End of battle") 
+            {
+                short distance = short.Parse(input);
+
+                if (energy>=distance)
+                {
+                    energy -= distance;
+                    battlesCount++;
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {battlesCount} won battles and {energy} energy");
+                    return;
+                }
+
+                if (battlesCount % 3 == 0)
+                {
+                    energy += battlesCount;
+                }
+            }
+
+            Console.WriteLine($"Won battles: {battlesCount}. Energy left: {energy}");
+        }
+    }
+}
