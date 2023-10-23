23a7844d8cdb25055c97e5b1762d246adc0924d4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-23 14:36:51 +0300

    Created Big Factorial For Loop and changed name

---
+++ 11.Objects and Classes/Objects and Classes - Lab/02. Big Factorial For Loop.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a number N in the range[0…1000]. Calculate the Factorial of N and print out the result.
+
+            short n = short.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
