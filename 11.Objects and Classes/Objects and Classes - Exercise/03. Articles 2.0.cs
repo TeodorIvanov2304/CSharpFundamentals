c5dbb0b0cda1491a3e8a382e2ac20d13e0588413
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-25 13:27:07 +0300

    Create 03. Articles 2.0.cs

---
+++ 11.Objects and Classes/Objects and Classes - Exercise/03. Articles 2.0.cs
@@ -0,0 +1,44 @@
+﻿namespace _03._Articles_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Article> articles = new List<Article>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine()
+                                     .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string title = data[0];
+                string content = data[1];
+                string author = data[2];
+                Article article = new Article(title, content, author);
+                articles.Add(article);
+            }
+            foreach (Article article in articles) 
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+        
+    }
+}
