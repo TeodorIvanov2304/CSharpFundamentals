a2d2acc69ebcbd2325029ba9d3171f355b4957f5
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-25 13:51:02 +0300

    Create 04. Students.cs

---
+++ 11.Objects and Classes/Objects and Classes - Exercise/04. Students.cs
@@ -0,0 +1,47 @@
+﻿namespace _04._Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            List<Student> studentList = new List<Student>();
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string firstName = data[0];
+                string lastName = data[1];
+                double grade = double.Parse(data[2]);
+
+                Student student = new Student(firstName, lastName, grade);
+                studentList.Add(student);
+            }
+
+            List<Student> filteredStudents = studentList.OrderBy(student => student.Grade).ToList();
+            filteredStudents.Reverse();
+
+            foreach(Student student in filteredStudents) 
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+    }
+
+    class Student
+    {   
+        public Student(string firstName, string lastName, double grade)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+}
