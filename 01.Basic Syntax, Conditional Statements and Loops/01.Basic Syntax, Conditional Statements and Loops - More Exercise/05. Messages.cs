85a15bb1225203d2309eb5bfe0f3b1f354951c36
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-10-27 16:19:31 +0300

    Create 05. Messages.cs

---
+++ 01.Basic Syntax, Conditional Statements and Loops/01.Basic Syntax, Conditional Statements and Loops - More Exercise/05. Messages.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05._Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfLetters = int.Parse(Console.ReadLine());
+            string word = string.Empty;
+
+            for (int i = 1; i <= countOfLetters; i++)
+            {
+                string currentDigit = Console.ReadLine(); 
+                int mainDigit = int.Parse(currentDigit[0].ToString());
+
+                if (mainDigit == 8 || mainDigit == 9)
+                {
+                    int offset = ((mainDigit - 2) * 3) + 1;
+                    int offsetInSquare = currentDigit.Length;
+                    int totalOffset = offset + offsetInSquare - 1;
+                    char letter = (char)(97 + totalOffset);
+                    word += letter;
+                }
+                else if (mainDigit == 0)
+                {
+                    word += ' ';
+                }
+                else
+                {
+                    int offset = (mainDigit - 2) * 3;
+                    int offsetInSquare = currentDigit.Length;
+                    int totalOffset = offset + offsetInSquare - 1;
+                    char letter = (char)(97 + totalOffset);
+                    word += letter;
+                }
+            }
+            Console.WriteLine(word);
+
+        }
+    }
+}
