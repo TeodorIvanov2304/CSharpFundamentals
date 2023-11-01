591139fe1ed10a6bd01dcedb00159d1396cc8fc1
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-01 16:17:39 +0200

    Create 03. Word Synonyms Second Variant.cs

---
+++ 13.Associative Arrays/Associative Arrays - Lab/03. Word Synonyms Second Variant.cs
@@ -0,0 +1,31 @@
+﻿namespace _03._Word_Synonyms_Second_Variant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var dictionary = new Dictionary<string,string> ();
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synoym = Console.ReadLine();
+
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = synoym;
+                }
+                else
+                {
+                    dictionary[word] += ", " + synoym;
+                }
+            }
+
+            foreach (KeyValuePair<string,string>kvp in dictionary)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+    }
+}
