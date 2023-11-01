9f61b03891443b3a9664c7dfe27fc43d0e99bffd
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-01 14:09:49 +0200

    Create 02. Odd Occurrences.cs

---
+++ 13.Associative Arrays/Associative Arrays - Lab/02. Odd Occurrences.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                             .Split()
+                             .Select(x => x.ToLower())
+                             .ToArray();
+
+            Dictionary<string,int> extractedWords = new Dictionary<string,int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                if (!extractedWords.ContainsKey(currentWord))
+                {
+                    extractedWords[currentWord] = 1;
+                }
+                else
+                {
+                    extractedWords[currentWord]++;
+                }
+            }
+
+            foreach(KeyValuePair<string,int> keyValuePair in extractedWords) 
+            {
+                if (keyValuePair.Value % 2 != 0) 
+                {
+                    Console.Write($"{keyValuePair.Key} ");
+                }
+            }
+        }
+    }
+}
