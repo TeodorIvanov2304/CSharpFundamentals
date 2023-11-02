773cc219c89644650261c069957522b82db4758f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-02 16:13:00 +0200

    Create 04. Word Filter One Line.cs

---
+++ 13.Associative Arrays/Associative Arrays - Lab/04. Word Filter One Line.cs
@@ -0,0 +1,13 @@
+﻿namespace _04._Word_Filter_One_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join("\n", Console.ReadLine()
+                                 .Split()
+                                 .Where(s => s.Length % 2 == 0)
+                                 .ToList()));
+        }
+    }
+}
