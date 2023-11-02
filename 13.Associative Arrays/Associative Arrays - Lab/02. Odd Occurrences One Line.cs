eaa064496b3a697137638f7336cd3521e0da845f
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-02 16:23:53 +0200

    Create 02. Odd Occurrences One Line.cs

---
+++ 13.Associative Arrays/Associative Arrays - Lab/02. Odd Occurrences One Line.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Odd_Occurrences_One_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", Console.ReadLine()
+                                                .Split()
+                                                .Select(x=>x.ToLower())
+                                                .GroupBy(x=>x)
+                                                .Where(x=>x.Count() % 2 == 1)
+                                                .SelectMany(x=>x)
+                                                .Distinct()
+                                                .ToList()));
+        }
+    }
+}
