1e70a6fd81a7dce185fa8ee793acff41299f37e2
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-02 16:05:31 +0200

    Create 04. Word Filter.cs

---
+++ 13.Associative Arrays/Associative Arrays - Lab/04. Word Filter.cs
@@ -0,0 +1,15 @@
+﻿namespace _04._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine()
+                                 .Split()
+                                 .Where(s => s.Length % 2 == 0)
+                                 .ToList();
+
+            Console.WriteLine(string.Join("\n", words));
+        }
+    }
+}
