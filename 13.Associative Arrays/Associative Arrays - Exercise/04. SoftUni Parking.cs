0a4711fd6f01f817934b5b2fdd1a65ecc4161071
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-04 22:26:27 +0200

    Create 04. SoftUni Parking.cs

---
+++ 13.Associative Arrays/Associative Arrays - Exercise/04. SoftUni Parking.cs
@@ -0,0 +1,51 @@
+﻿namespace _04._SoftUni_Parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> database = new Dictionary<string, string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string commands = Console.ReadLine();
+                string[] commandsTokens = commands.Split();
+                string command = commandsTokens[0];
+                string username = commandsTokens[1];
+                switch (command)
+                {
+                    case "register":
+                        string licensplate = commandsTokens[2];
+                        if (!database.ContainsKey(username))
+                        {
+                            database.Add(username, licensplate);
+                            Console.WriteLine($"{username} registered {licensplate} successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: already registered with plate number {licensplate}");
+                        }
+                        break;
+                    case "unregister":
+                        if (!database.ContainsKey(username))
+                        {
+                            Console.WriteLine($"ERROR: user {username} not found");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{username} unregistered successfully");
+                            database.Remove(username);
+                        }
+                        break;
+                }
+
+            }
+
+            foreach (KeyValuePair<string, string> kvp in database)
+            {
+                Console.WriteLine($"{kvp.Key} => {kvp.Value}");
+            }
+        }
+    }
+}
