f9c44f80b12b48cd5d6c87818373e27ca650b524
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-04 13:40:30 +0200

    Created Dictionaries Folder and Count Chars Problem

---
+++ 13.Associative Arrays/Associative Arrays - Exercise/01. Count Chars in a String.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> text = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentSymbol = input[i];
+                if (!text.ContainsKey(currentSymbol) && currentSymbol != ' ')
+                {
+                    text[currentSymbol] = 1;
+                }
+                else if(text.ContainsKey(currentSymbol) && currentSymbol != ' ') 
+                {
+                    text[currentSymbol]++;
+                }
+            }
+
+            Dictionary<char, int> ordered = new Dictionary<char, int>();
+            text.OrderBy(c => c);
+
+            foreach(KeyValuePair<char,int> kvp in text)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
