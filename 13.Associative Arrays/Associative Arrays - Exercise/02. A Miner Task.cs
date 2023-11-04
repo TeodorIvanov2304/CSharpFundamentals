dd995c04331cef56f64389d424f0213599bdacb4
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-04 14:53:54 +0200

    Create 02. A Miner Task.cs

---
+++ 13.Associative Arrays/Associative Arrays - Exercise/02. A Miner Task.cs
@@ -0,0 +1,32 @@
+﻿namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            
+            Dictionary<string,int> resources = new Dictionary<string, int>();
+            string resource = "";
+            while ((resource = Console.ReadLine()) != "stop") 
+            {   
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources[resource] = quantity;
+                }
+                else
+                {
+                    resources[resource] += quantity;
+                }
+
+            }
+
+            foreach(KeyValuePair<string, int> kvp in resources) 
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
