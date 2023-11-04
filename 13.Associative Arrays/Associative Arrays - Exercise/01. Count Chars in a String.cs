6434059615c8debcef138282cb8dea261a12874a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-04 15:00:51 +0200

    Update 01. Count Chars in a String.cs

--- 13.Associative Arrays/Associative Arrays - Exercise/01. Count Chars in a String.cs
+++ 13.Associative Arrays/Associative Arrays - Exercise/01. Count Chars in a String.cs
@@ -7,25 +7,26 @@ namespace _01._Count_Chars_in_a_String
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-            Dictionary<char, int> text = new Dictionary<char, int>();
+            Dictionary<char, int> characterOccurances = new Dictionary<char, int>();
 
             for (int i = 0; i < input.Length; i++)
             {
                 char currentSymbol = input[i];
-                if (!text.ContainsKey(currentSymbol) && currentSymbol != ' ')
+                if (!characterOccurances.ContainsKey(currentSymbol) && currentSymbol != ' ')
                 {
-                    text[currentSymbol] = 1;
+                    characterOccurances[currentSymbol] = 1;
+                    //characterOccurances.Add(currentSymbol,1);
                 }
-                else if(text.ContainsKey(currentSymbol) && currentSymbol != ' ') 
+                else if(characterOccurances.ContainsKey(currentSymbol) && currentSymbol != ' ') 
                 {
-                    text[currentSymbol]++;
+                    characterOccurances[currentSymbol]++;
                 }
             }
 
             Dictionary<char, int> ordered = new Dictionary<char, int>();
-            text.OrderBy(c => c);
+            characterOccurances.OrderBy(c => c);
 
-            foreach(KeyValuePair<char,int> kvp in text)
+            foreach(KeyValuePair<char,int> kvp in characterOccurances)
             {
                 Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
             }
