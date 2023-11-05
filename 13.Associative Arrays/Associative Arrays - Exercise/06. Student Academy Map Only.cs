906162bc18dae5480391a544c4bb0d43472ed83b
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-05 12:37:15 +0200

    Create 06. Student Academy Map Only.cs

---
+++ 13.Associative Arrays/Associative Arrays - Exercise/06. Student Academy Map Only.cs
@@ -0,0 +1,37 @@
+﻿
+namespace _06._Student_Academy_Map_Only
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < rows; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(name))
+                {
+                    students[name] = new List<double>
+                    {
+                        grade
+                    };
+                }
+                else
+                {
+                    students[name].Add(grade);
+                }
+
+            }
+
+            var filteredStudents = students.Where(g => g.Value.Average() >= 4.50);
+
+            foreach (KeyValuePair<string,List<double>> student in filteredStudents) 
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value.Average():f2}");
+            }
+        }
+    }
+}
