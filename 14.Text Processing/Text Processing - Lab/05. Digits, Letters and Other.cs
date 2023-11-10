83072c620d499c8655a7945104557b9860016bb6
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-11 01:22:52 +0200

    Create 05. Digits, Letters and Other.cs

---
+++ 14.Text Processing/Text Processing - Lab/05. Digits, Letters and Other.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder digits = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder symbols = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {   
+                char currentSymbol = text[i];
+                if (char.IsDigit(currentSymbol))
+                {
+                    digits.Append(currentSymbol).ToString();
+                }
+                else if (char.IsLetter(currentSymbol))
+                {
+                    letters.Append(currentSymbol);
+                }
+                else
+                {
+                    symbols.Append(currentSymbol);
+                }
+
+            }
+            
+            Console.WriteLine($"{digits}\n{letters}\n{symbols}");
+        }
+    }
+}
