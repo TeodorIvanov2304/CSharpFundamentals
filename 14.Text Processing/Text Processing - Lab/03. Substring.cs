dcb80cba0cd3118b96f54d7ea1a4362aafec1b56
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-10 22:53:08 +0200

    Create 03. Substring.cs

---
+++ 14.Text Processing/Text Processing - Lab/03. Substring.cs
@@ -0,0 +1,21 @@
+﻿namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            // Въртим цикъла, докато filter се съдържа в text
+            while (text.Contains(filter)) 
+            {   
+                // Премахваме частта от текст(filter) от text с метода Remove, и IndexOf
+                // Където filter е началният индекс, от който премахваме
+                // До filter.Length , т.е докато свърши филтъра
+                text = text.Remove(text.IndexOf(filter), filter.Length);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
