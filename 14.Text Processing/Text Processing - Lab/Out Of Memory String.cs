a48a27c31d2aa6389d846ca8cf4898b7dfa2df62
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-10 22:11:53 +0200

    Create Out Of Memory String.cs

---
+++ 14.Text Processing/Text Processing - Lab/Out Of Memory String.cs
@@ -0,0 +1,17 @@
+﻿namespace _01._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = new string(new char[] { 'a', 'b', 'c' });
+
+            for (int i = 0; i < 100; i++)
+            {
+                name += name;
+                Console.WriteLine(i);
+            }
+            
+        }
+    }
+}
