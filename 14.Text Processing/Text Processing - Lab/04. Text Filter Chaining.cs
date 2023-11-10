2c9727971b74a17d845c544b05b43b83c1a2504a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-11 00:07:02 +0200

    Create 04. Text Filter Chaining.cs

---
+++ 14.Text Processing/Text Processing - Lab/04. Text Filter Chaining.cs
@@ -0,0 +1,20 @@
+﻿namespace _04._Text_Filter_Chaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                                   .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                                   .ToArray();
+            string text = Console.ReadLine();
+
+            foreach (string word in bannedWords) 
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
