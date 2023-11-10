868c598faf4c756a0eb0546e7b214284abc5d807
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-11 00:28:53 +0200

    Create Stopwatch StringBuilder vs. Concata.cs

---
+++ 14.Text Processing/Text Processing - Lab/Stopwatch StringBuilder vs. Concata.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Stopwatch_StringBuilder_vs._Concata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string text = "";
+            for (int i = 0; i < 200000; i++)
+            {
+                text += i;
+                
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+            Stopwatch stopwatch2 = new Stopwatch();
+            stopwatch2.Start();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 200000; i++)
+            {
+                stringBuilder.Append(i);
+            }
+            stopwatch2.Stop();
+            Console.WriteLine(stopwatch2.ElapsedMilliseconds);
+        }
+    }
+}
