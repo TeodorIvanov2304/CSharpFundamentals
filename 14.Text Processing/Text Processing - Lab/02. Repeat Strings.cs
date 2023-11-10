3f3a848196479996fcd67e40c241ea2d3008c694
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-10 22:21:01 +0200

    Create 02. Repeat Strings.cs

---
+++ 14.Text Processing/Text Processing - Lab/02. Repeat Strings.cs
@@ -0,0 +1,23 @@
+﻿namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .ToArray();
+            string allWords = "";
+            foreach (string word in words) 
+            {
+                
+                int repeats = word.Length;
+                for (int i = 0; i < repeats; i++)
+                {
+                    allWords += word;
+                }
+            }
+            Console.WriteLine(allWords);
+        }
+    }
+}
