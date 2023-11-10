9b3aaf1c58c905d04d5c2dc52553af633fb21e34
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-10 23:26:47 +0200

    Create 04. Text Filter.cs

---
+++ 14.Text Processing/Text Processing - Lab/04. Text Filter.cs
@@ -0,0 +1,30 @@
+﻿namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                 .ToArray();
+            string text  = Console.ReadLine();
+
+            foreach (string word in bannedWords) 
+            {
+                string newWord = "";
+                
+                for (int i = 0; i < word.Length; i++)
+                {
+                    newWord += "*";
+                }
+                while (text.Contains(word)) 
+                {
+
+                    text = text.Replace(word,newWord);
+                }
+            }
+            Console.WriteLine(text);
+
+        }
+    }
+}
