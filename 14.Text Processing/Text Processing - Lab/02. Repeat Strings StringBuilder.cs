56eecc5598c2bd859365347132cc53d85eb06ce8
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-10 22:25:48 +0200

    Create 02. Repeat Strings StringBuilder.cs

---
+++ 14.Text Processing/Text Processing - Lab/02. Repeat Strings StringBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace _02._Repeat_Strings_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                             .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                             .ToArray();
+
+            StringBuilder result = new StringBuilder();
+            foreach (string str in input) 
+            {
+                int index = str.Length;
+                for (int i = 0; i < index; i++)
+                {
+                    result.Append(str);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
