953d696f04a5aaeaf5f03fc7d367ec90111ddeca
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-11 19:33:40 +0200

    Create Text Processing Exercise Folder and  Valid Usernames Problem

---
+++ 14.Text Processing/Text Processing - Exercise/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                 .ToArray();
+
+            foreach (string username in usernames)
+            {
+
+
+                if (username.Length < 3 || username.Length > 16)
+                {
+                    continue;
+                }
+
+                if (isValidUserName(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+
+        }
+
+        //Създаваме bool метод
+        public static bool isValidUserName(string username)
+        {
+            return username.All(ch => char.IsLetterOrDigit(ch)
+                                                      || ch == '_'
+                                                      || ch == '-');
+        }
+    }
+}
