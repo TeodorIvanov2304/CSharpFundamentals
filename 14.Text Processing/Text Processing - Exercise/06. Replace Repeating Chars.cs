fab6ce302dd1be1955e3521ca02b22cd87a4c970
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-12 16:49:40 +0200

    Create 06. Replace Repeating Chars.cs

---
+++ 14.Text Processing/Text Processing - Exercise/06. Replace Repeating Chars.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                if (input[i] != input[i + 1])
+                {
+                    result.Append(input[i]);
+                }
+            }
+
+            // Append the last character
+            result.Append(input[input.Length - 1]);
+
+            string output = result.ToString();
+            Console.WriteLine(output);
+        }
+    }
+}
