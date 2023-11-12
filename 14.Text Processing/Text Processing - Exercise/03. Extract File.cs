9dd974bee8ca00f7869560692144b17adee44e4d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-12 10:40:28 +0200

    Create 03. Extract File.cs

---
+++ 14.Text Processing/Text Processing - Exercise/03. Extract File.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+
+            StringBuilder fileName = new StringBuilder();
+            StringBuilder extension = new StringBuilder();
+
+            int lastDashIndex = filePath.LastIndexOf("\\");
+            int dotIndex = filePath.IndexOf(".");
+            for (int i = lastDashIndex + 1; i < dotIndex; i++)
+            {
+
+                fileName.Append(filePath[i]);
+            }
+
+            for (int i = dotIndex+1; i < filePath.Length; i++)
+            {
+                extension.Append(filePath[i]);
+            }
+
+            
+            Console.WriteLine($"File name: {fileName}\nFile extension: {extension}");
+        }
+    }
+}
