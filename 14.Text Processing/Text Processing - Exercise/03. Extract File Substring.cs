3a46d1bc74dab902e2b45d08282e775fc2d19b5d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-12 11:44:27 +0200

    Update 03. Extract File Substring.cs

--- 14.Text Processing/Text Processing - Exercise/03. Extract File Substring.cs
+++ 14.Text Processing/Text Processing - Exercise/03. Extract File Substring.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = Console.ReadLine();
+
+            string fileName = string.Empty;
+            string fileExtension = string.Empty;
+
+            int lastSeparatorIndex = filePath.LastIndexOf('\\');
+            int extensionIndex = filePath.LastIndexOf(".");
+            
+            if (lastSeparatorIndex != -1 
+                && extensionIndex != -1 
+                && extensionIndex>lastSeparatorIndex) 
+            {
+                fileName = filePath.Substring(lastSeparatorIndex + 1, 
+                    extensionIndex - lastSeparatorIndex - 1);
+                fileExtension = filePath.Substring(extensionIndex+1);
+            }
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
         }
     }
 }
