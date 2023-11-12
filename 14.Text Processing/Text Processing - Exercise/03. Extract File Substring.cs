a8c748b631f11ddde6d101d16843d2b3736b85e9
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-12 11:43:43 +0200

    Create 03. Extract File Substring.cs

---
+++ 14.Text Processing/Text Processing - Exercise/03. Extract File Substring.cs
@@ -0,0 +1,10 @@
+﻿namespace _03._Extract_File_Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
