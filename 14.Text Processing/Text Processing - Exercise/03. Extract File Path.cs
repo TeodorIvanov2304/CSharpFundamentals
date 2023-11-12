13f56df6f0cc0c72719a6d1f537eee29fd3ff2a3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-12 12:01:19 +0200

    Create 03. Extract File Path.cs

---
+++ 14.Text Processing/Text Processing - Exercise/03. Extract File Path.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace _03._Extract_File_Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+            //Използваме вградения клас Path и функциите му GetFileNameWithoutExtension и GetExtension
+            Console.WriteLine($"File name: {Path.GetFileNameWithoutExtension(filePath)}");
+            //Реплейсваме . с празно място
+            Console.WriteLine($"File extension: {Path.GetExtension(filePath).Replace(".","")}");
+        }
+    }
+}
