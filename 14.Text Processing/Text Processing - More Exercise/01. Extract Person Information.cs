c42ad6bdbaa4be60522bac8242e3e5e0feee5406
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-13 19:06:42 +0200

    Created More Exercise folder and 01 Problem

---
+++ 14.Text Processing/Text Processing - More Exercise/01. Extract Person Information.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPersons = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < numberOfPersons; i++)
+            {
+              
+                    string input = Console.ReadLine();
+                    int startIndexName = input.IndexOf('@');
+                    int endIndexName = input.IndexOf('|');
+                    string name = input.Substring(startIndexName + 1, endIndexName - startIndexName - 1);
+                    int startIndexAge = input.IndexOf('#');
+                    int endIndexAge = input.IndexOf('*');
+                    string ageStr = input.Substring(startIndexAge + 1, endIndexAge - startIndexAge - 1);
+                    Console.WriteLine($"{name} is {ageStr} years old.");
+
+            
+        }
+    }
+}
