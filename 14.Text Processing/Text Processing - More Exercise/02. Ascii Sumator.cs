bd51b693fb872dd4b75172c9c9d9e725ec70178d
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-14 10:34:37 +0200

    Create 02. Ascii Sumator.cs

---
+++ 14.Text Processing/Text Processing - More Exercise/02. Ascii Sumator.cs
@@ -0,0 +1,24 @@
+﻿namespace _02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char start = char.Parse(Console.ReadLine());
+            char end  = char.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentSymbol = input[i];
+                if (currentSymbol > start && currentSymbol < end)
+                {
+                    sum += (int)currentSymbol;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
