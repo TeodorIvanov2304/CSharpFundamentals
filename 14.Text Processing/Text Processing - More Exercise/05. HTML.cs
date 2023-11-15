9b0306692e8503a8c30b1f2e2de96ef6f4ec4cc6
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-15 15:09:12 +0200

    Create 05. HTML.cs

---
+++ 14.Text Processing/Text Processing - More Exercise/05. HTML.cs
@@ -0,0 +1,43 @@
+﻿namespace _05._HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string titleAndArticle = Console.ReadLine();
+
+
+            string content = Console.ReadLine();
+            string comment = "";
+            List<string> comments = new List<string>();
+
+            while ((comment = Console.ReadLine()) != "end of comments")
+            {
+
+                comments.Add(comment);
+            }
+            PrintTitle(titleAndArticle);
+            PrintContent(content);
+            PrintComments(comments);
+        }
+
+        public static void PrintComments(List<string> comments)
+        {
+            foreach (string comment in comments) 
+            {
+                Console.WriteLine($"<div>\n    {comment}\n</div>");
+            }
+        }
+
+        private static void PrintContent(string content)
+        {
+            Console.WriteLine($"<article>\n    {content}\n</article>");
+        }
+
+        public static void PrintTitle(string title)
+        {
+            Console.WriteLine($"<h1>\n    {title}\n</h1>");
+        }
+    }
+}
+
