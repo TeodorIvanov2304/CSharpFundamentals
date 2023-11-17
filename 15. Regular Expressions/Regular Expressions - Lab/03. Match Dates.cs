48a6a250cce56150590755d55ba3ed7e3deab263
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-17 21:07:42 +0200

    Create 03. Match Dates.cs

---
+++ 15. Regular Expressions/Regular Expressions - Lab/03. Match Dates.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _03._Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            string input = Console.ReadLine();
+
+            foreach (Match m in Regex.Matches(input, pattern))
+            {
+                Console.WriteLine($"Day: {m.Groups["day"]}, Month: {m.Groups["month"]}, Year: {m.Groups["year"]}");
+            }
+        }
+    }
+}
