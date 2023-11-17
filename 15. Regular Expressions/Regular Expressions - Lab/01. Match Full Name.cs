b9c78e331077e5e1bb8885c82addcdb106e9d390
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-17 14:45:56 +0200

    Created folder and 2 regex problems

---
+++ 15. Regular Expressions/Regular Expressions - Lab/01. Match Full Name.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Regex for finding First and Last name
+            string pattern = @"\b([A-Z][a-z]+) ([A-Z][a-z]+)";
+            string input = Console.ReadLine();
+
+            foreach (Match m in Regex.Matches(input, pattern))
+            {
+                Console.Write($"{m.Value} ");
+            }
+        }
+    }
+}
---
+++ 15. Regular Expressions/Regular Expressions - Lab/02. Match Phone Number.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+([359]+)([- ])2(\2)(\d{3})(\2)(\d{4})\b";
+            string phoneInputs = Console.ReadLine();
+            Regex phonePatters = new Regex(pattern);
+            
+
+            var matches = phonePatters.Matches(phoneInputs);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
