29fd02650ae456901e0cfdd2b51f46f6e19c7cc1
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-20 23:37:31 +0200

    Update 03. SoftUni Bar Income.cs

--- 15. Regular Expressions/Regular Expressions - Exercise/03. SoftUni Bar Income.cs
+++ 15. Regular Expressions/Regular Expressions - Exercise/03. SoftUni Bar Income.cs
@@ -6,22 +6,19 @@ namespace _03._SoftUni_Bar_Income
     {
         static void Main(string[] args)
         {
-            string pattern = @"\W(?<Name>[A-Z][a-z]+)\W+(?<Product>[A-Z][a-z]+)\W+(\D*|W+)(?<Count>[0-9]+)\W+(?<Price>[0-9]+\.[0-9]|[A-z]+\d+|\W*)\$";
+            string pattern = @"\%([A-Z][a-z]+)\%[^|$%.]*\<(\w+)\>[^|$%.]*\|(\d+)\|[^|$%.]*?(\d+(?:\.\d+)?)\$";
             double totalPrice = 0;
             string input = "";
             while ((input = Console.ReadLine()) != "end of shift") 
             {
                 foreach (Match item in Regex.Matches(input,pattern))
                 {
-                    string name = item.Groups["Name"].Value;
-                    string product = item.Groups["Product"].Value;
-                    uint count = uint.Parse(item.Groups["Count"].Value);
-                    double price = 0;
-                    string numberPattern = @"[^\.\d]";
-                    string number = Regex.Replace(item.Groups["Price"].Value, numberPattern, "").ToString();
-
-                    price = double.Parse(number);
+                    string name = item.Groups[1].Value;
+                    string product = item.Groups[2].Value;
+                    uint count = uint.Parse(item.Groups[3].Value);
+                    double price = double.Parse(item.Groups[4].Value);
                     
+
                     Console.WriteLine($"{name}: {product} - {price*count:F2}");
                     totalPrice += price*count;
                 }
