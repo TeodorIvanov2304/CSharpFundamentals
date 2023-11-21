57f851c8cad88baed3753ef3985ba1bdeca77db9
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-21 15:50:58 +0200

    Update 04. Star Enigma 2nd Variation.cs

--- 15. Regular Expressions/Regular Expressions - Exercise/04. Star Enigma 2nd Variation.cs
+++ 15. Regular Expressions/Regular Expressions - Exercise/04. Star Enigma 2nd Variation.cs
@@ -1,17 +1,6 @@
 ﻿using System.Text;
 using System.Text.RegularExpressions;
 
-/*
-2
-STCDoghudd4=63333$D$0A53333
-EHfsytsnhf?8555&I&2C9555SR
-
-3
-tt(''DGsvywgerx>6444444444%H%1B9444
-GQhrr|A977777(H(TTTT
-EHfsytsnhf?8555&I&2C9555SR
-*/
-
 class Message
 {
     public Message()
