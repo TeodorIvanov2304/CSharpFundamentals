cfac079c9b64f256f9101b1c3044de44fa47463a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-11-23 01:42:42 +0200

    Create 06. Extract Emails.cs

---
+++ 15. Regular Expressions/Regular Expressions - Exercise/06. Extract Emails.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _06._Extract_Emails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            //string pattern = @"(?'User'[a-z\._-]?)+@(?'Host'[a-z-\.]+)";    33/100
+            //Регекс за извличане на имейли
+            string pattern = @"((?<=\s)[a-zA-Z0-9]+([-.]\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\.[a-z]{2,}))";
+            MatchCollection emails = Regex.Matches(input, pattern);
+            foreach (Match match in emails)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
