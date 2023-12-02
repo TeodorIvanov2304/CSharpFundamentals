4f2308ac841a29a7078d01e67d7cbd4a3b7cee78
T.Ivanov2304 <t.ivanov2304@gmail.com>
2023-12-02 16:08:22 +0200

    Create 04. Raw Data.cs

---
+++ 11.Objects and Classes/Objects and Classes - More Exercise/04. Raw Data.cs
@@ -0,0 +1,79 @@
+﻿namespace _04._Raw_Data
+{
+    class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+        }
+
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+    }
+    class Engine
+    {
+        public Engine(uint engineSpeed, uint enginePower)
+        {
+            EngineSpeed = engineSpeed;
+            EnginePower = enginePower;
+        }
+
+        public uint EngineSpeed { get; set; }
+        public uint EnginePower { get; set; }
+    }
+
+    class Cargo
+    {
+        public Cargo(uint weight, string cargoType)
+        {
+            Weight = weight;
+            CargoType = cargoType;
+        }
+
+        public uint Weight { get; set; }
+        public string CargoType { get; set; }
+    }
+    internal class Program
+    {   
+        
+        static void Main(string[] args)
+        {   
+            List<Car> carList = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string carInfo = Console.ReadLine();
+                string[] carInfoArgs = carInfo.Split().ToArray();
+                //"<Model> <EngineSpeed> <EnginePower> <CargoWeight> <CargoType>"
+                
+                uint engineSpeed = uint.Parse(carInfoArgs[1]);
+                uint enginePower = uint.Parse(carInfoArgs[2]);
+                Engine engine = new Engine(engineSpeed, enginePower);
+
+                uint cargoWeight = uint.Parse(carInfoArgs[3]);
+                string cargoType = carInfoArgs[4];
+                Cargo cargo = new Cargo(cargoWeight, cargoType);
+
+                string carModel = carInfoArgs[0];
+                Car car = new Car(carModel, engine, cargo);
+                carList.Add(car);
+            }
+
+            string cargoTypeToCheck = Console.ReadLine();
+            foreach (Car car in carList.Where(c=>c.Cargo.CargoType == cargoTypeToCheck))
+            {
+                if (cargoTypeToCheck == "fragile" && car.Cargo.Weight<1000)
+                {
+                    Console.WriteLine($"{car.Model}");
+                }
+                else if(cargoTypeToCheck == "flamable" && car.Engine.EnginePower > 250)
+                {
+                    Console.WriteLine($"{car.Model}");
+                }
+            }
+        }
+    }
+}
